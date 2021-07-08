2b84f77256c2af0f43bcdd57c8b1a281a384bd94
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2021-07-09 00:53:47 +0300

    Sorter alpha

--- Assets/Scripts/UI/VacuumUI.cs
+++ Assets/Scripts/UI/VacuumUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -13,14 +14,39 @@ public class VacuumUI : MonoBehaviour
     [SerializeField] private Transform mRoot;
     [SerializeField] private EngineVacuumDrawer mEngineVacuumDrawer;
 
+    [Space]
+    [SerializeField] private Button mEngineMass;
+    [SerializeField] private Button mMaxDeltaV;
+    [SerializeField] private Button mImpulse;
+    [SerializeField] private Button mEngineThrust;
+    [SerializeField] private Button mAlias;
+    [SerializeField] private Button mThrust;
+    [SerializeField] private Button mCost;
+    [SerializeField] private Button mMass;
+    [SerializeField] private Button mTime;
+    [SerializeField] private Button mFuelMass;
+    [SerializeField] private Button mFuelTankMass;
+
+
     public List<Technology> Technologies { get; set; } = new List<Technology>();
 
     //TODO Major Суммирование нескольких DeltaV + домножение + загрузка в PlayerPrefs
-    //TODO Major Сортировка
     //TODO Major Подсветка оптимальных
 
     void Start()
     {
+        mEngineMass.onClick.AddListener(() => Sort(d => d.EngineMass));
+        mMaxDeltaV.onClick.AddListener(() => Sort(d => d.MaxDeltaV));
+        mImpulse.onClick.AddListener(() => Sort(d => d.Impulse));
+        mEngineThrust.onClick.AddListener(() => Sort(d => d.EngineThrust));
+        mAlias.onClick.AddListener(() => Sort(d => d.EngineAlias));
+        mThrust.onClick.AddListener(() => Sort(d => d.Thrust));
+        mCost.onClick.AddListener(() => Sort(d => d.Cost));
+        mMass.onClick.AddListener(() => Sort(d => d.Mass));
+        mTime.onClick.AddListener(() => Sort(d => d.Time));
+        mFuelMass.onClick.AddListener(() => Sort(d => d.FuelMass));
+        mFuelTankMass.onClick.AddListener(() => Sort(d => d.FuelTankMass));
+
         mPayload.onEndEdit.AddListener(_ => Calculate());
         mDeltaV.onEndEdit.AddListener(_ => Calculate());
 
@@ -49,5 +75,12 @@ public class VacuumUI : MonoBehaviour
             drawer.DeltaV = deltaV;
             drawer.Calculate();
         }
+    }
+
+    private void Sort<T>(Func<EngineVacuumDrawer, T> sorter)
+    {
+        var order = 0;
+        foreach (var drawer in mEngineVacuumDrawers.OrderBy(sorter))
+            drawer.transform.SetSiblingIndex(order++);
     }
 }
